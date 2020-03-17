3c4b66edd5d24970ec6c7df1860adbc74dabcc99
Logan Negley <logannegley@gmail.com>
2020-03-17 18:47:20 -0500

    UpdateForm component created

---
+++ client/src/UpdateForm.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+function UpdateForm() {
+  
+  return (
+    <div className='updateForm'>
+      
+    </div>
+  );
+}
+
+export default UpdateForm;
