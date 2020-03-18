d9036fbb255b0a230f2811f99136395b26b9c08f
Logan Negley <logannegley@gmail.com>
2020-03-17 19:41:27 -0500

    form inputs and handleChanges done

--- client/src/UpdateForm.js
+++ client/src/UpdateForm.js
@@ -10,20 +10,62 @@ const initialMovie = {
 };
 
 function UpdateForm() {
-    const [movie, setMovie]= useState(initalMovie)
+    const [movie, setMovie]= useState(initialMovie)
   
+//   handle changes function in form
+const handleChange = event => {
+    let value = event.target.value;
+    if(event.target.name === 'metascore'){
+        value = parseInt(value, 10);
+    }
+
+    setMovie({
+        ...movie,
+        [event.target.name]: value
+    })
+};
+
+const handleSubmit = ()=>{
+
+};
+
+
   return (
     <div className='updateForm'>
-      
+      <h2>Update Movie Information</h2>
+      <form>
+          <input
+              type='text'
+              name='title'
+              placeholder="Title"
+            //   onChange={}
+            value={movie.title}
+          />
+            <input
+              type='text'
+              name='director'
+              placeholder="Director"
+            //   onChange={}
+            value={movie.director}
+          />
+            <input
+              type='text'
+              name='metascore'
+              placeholder="Metascore"
+            //   onChange={}
+            value={movie.metascore}
+          />
+            <input
+              type='text'
+              name='stars'
+              placeholder="Actors"
+            //   onChange={}
+            value={movie.stars}
+          />
+          <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
 
 export default UpdateForm;
-
-
-
-// // Movie Title
-// // Director
-// metascore
-// stars/actors
